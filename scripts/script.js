25b243d9127da24d0b23a9abaf4833928404ceec
lars <m8tower@gmail.com>
2022-09-13 22:31:27 +0200

    working library skeleton - todo fix read status

--- scripts/script.js
+++ scripts/script.js
@@ -22,12 +22,23 @@ function closePopUp() {
 }
 
 
-//displays the books
+//displays myLibrary array inside id "bookContainer"
 function displayBooks() {
-
+    var bookContainer = document.getElementById("bookContainer");
+    bookContainer.innerHTML = "";
+    for (let i = 0; i < myLibrary.length; i++) {
+        bookContainer.innerHTML += "<div class='book'><h2>" + myLibrary[i].title + "</h2><p>" + myLibrary[i].author + "</p><p>" + myLibrary[i].pages + "</p><p>" + myLibrary[i].read + "</p><button class='delete' onclick='deleteBook(" + i + ")'>Delete</button></div>";
+    }
+    
     console.table(myLibrary);
-
 }
+
+//deletes book from myLibrary array
+function deleteBook(index) {
+    myLibrary.splice(index, 1);
+    displayBooks();
+}
+
 
 //Book constructor
 class Book {
@@ -52,6 +63,7 @@ document.querySelector("form").addEventListener("submit", e => {
     var title = document.getElementById("title").value;
     var author = document.getElementById("author").value;
     var pages = document.getElementById("pages").value;
+    var read = document.getElementById("read").value;
     var book = new Book(title, author, pages, read);
     myLibrary.push(book);
     displayBooks();
